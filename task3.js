03574da9bb36a4e1c7049a343d393f1abe7acad5
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-27 05:56:35 -0500

    task three added

---
+++ task3.js
@@ -0,0 +1,16 @@
+// Task-3
+// Your task is to divide the given number by 5 and show the remainder as the output.
+
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+var numberFromUser=parseInt(prompt("Enter your desirable number")),
+dividedNumber=5,remainder;
+console.log("The user given number is=",numberFromUser);
+
+remainder=numberFromUser%remainder;
+console.log("The remainder is =",remainder);
+
